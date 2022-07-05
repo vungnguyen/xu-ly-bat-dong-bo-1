00bb485d47439b42b155206f2aeedb5c3a6da526
vungnguyen <vungvovi@gmail.com>
2022-07-05 16:06:13 +0700

    ok

---
+++ App Time Countdown/timeCountdown.js
@@ -0,0 +1,15 @@
+function sleep(ms){
+    return new Promise((resolve) => setTimeout(resolve, ms))
+}
+async function countDown(i){
+    while(i > 0){
+        document.getElementById('time-countDown').innerHTML = i;
+        i--;
+        await sleep(1000);
+    }
+    return 'counter finished'
+}
+let counter = countDown(10);
+counter.then((msg) => {document.getElementById('time-countDown').innerHTML = msg})
+
+
